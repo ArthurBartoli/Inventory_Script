9d73fc43dc7086e4252ba4acff0faf843fccff4f
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-02-15 16:07:06 +0100

    Command arguments in lower case
    feat: the command arguments are put in lower case and compared as such

--- start.py
+++ start.py
@@ -25,25 +25,28 @@ list_args = [all_args[i * n:(i + 1) * n] for i in range((len(all_args) + n - 1) 
 # If the list of arguments is empty, we enter a dummy one
 if not all_args: list_args = ["no arguments"]
 
+print(list_args)
 export_bool = False
 for arg in list_args:
     match arg[0]:
-        # Does the script need to run the data export ?
-        case '--export' | "-e":
-            if arg[1] not in ["yes", "y", "no", "n"]:
+        case '--export' | "-e": # Does the script need to run the data export ?
+            arg_value = arg[1].lower()
+            if arg_value not in ["yes", "y", "no", "n"]:
                 print("Invalid argument. Please enter [y]es or [n]o.")
                 pass
             else:
-                export_bool = (arg[1] == "y" or arg[1] == "yes")
+                export_bool = (arg_value == "y" or arg_value == "yes")
         case "--help" | "-h":
             print("You have entered the help command, all further commands are ignored.")
             print("Here is a list of available options :")
             print("\t* --export|-e [Y]es|[N]o")
             print("\t\tWhether or not to run the powershell script which exports all data")
             print("\t\tfrom tenant. If the export is already done once, it is not necessary.")
+        case "no arguments":
+            pass
         case _:
             print("## Could not recognise the argument, please enter a valid option. ##")
-                            
+
 # Defining paths
 script_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_directory, "json_extract.ps1")
