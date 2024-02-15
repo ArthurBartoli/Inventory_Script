0e965e953a7cf03a4404718a3b6302e370233a11
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-02-15 11:05:05 +0100

    Add command line management + option to run export or not
    feat: Add option to run export or not which is managed in start.py via pattern matching. It tells the user to enter a valid option
    /!\ These arguments can only be passed to start.py for now, not the root of the software

--- start.py
+++ start.py
@@ -10,12 +10,32 @@ EXCEL NEEDS TO BE ALREADY OPENED FIRST
 '''
 
 import os
+import sys
 
 from toolbox.scan_export import scan_export
 from toolbox.ExcelManager import ExcelManager
 from toolbox.run_powershell_script import run_powershell_script
 from toolbox.unwrap_json import unwrap_json
 
+# Split the arguments list in pairs
+all_args = sys.argv[1:] # We skip the first argument which is the file name
+n = 2
+list_args = [all_args[i * n:(i + 1) * n] for i in range((len(all_args) + n - 1) // n )]  
+print(list_args)
+
+ps_bool = False
+
+for arg in list_args:
+    match arg[0]:
+        # Does the script need to run the data export ?
+        case '--export' | "-e":
+            if arg[1] not in ["yes", "y", "no", "n"]:
+                print("Invalid argument. Please enter [y]es or [n]o.")
+                pass
+            else:
+                export_bool = (arg[1] == "y" or arg[1] == "yes")
+        case _:
+            print("Please enter a valid option.")
                             
 # Defining paths
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +43,7 @@ file_path = os.path.join(script_directory, "json_extract.ps1")
 export_directory = os.path.join(script_directory, "export")
 export_path = os.path.join(script_directory, 'export.json')
 
-#run_powershell_script(file_path)
+if export_bool: run_powershell_script(file_path)
     
 json_data = unwrap_json(export_path)
 res_final = scan_export(json_data)
