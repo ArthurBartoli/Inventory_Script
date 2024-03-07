ba913d2eea07937a0f7974d9004d02ea98fcff1c
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-03-07 11:02:49 +0100

    Add Avalonia project structure
    chores: Avalonia solution added and the former part of the program is now in the Scripts folder

---
+++ PBI Inventory/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PBI_Inventory.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ PBI Inventory/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PBI_Inventory.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
